53efd9884df675a40d36ea83f2818304bddc5369
ranajasx <jassrana2005@gmail.com>
2025-05-03 01:07:26 +0530

    Delete TASK-1.py

--- TASK-1.py
+++
@@ -1,6 +0,0 @@
-a = int(input('Enter the first number: '))
-b = int(input('Enter the second number: '))
-print('addition',a+b)
-print('subtraction',a-b)
-print('multiplication',a*b)
-print('division',a/b)
